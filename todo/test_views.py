0e6d0a0fb079095324b492efc0497b274623335d
joffy69 <jcwgibbs@gmail.com>
2020-12-20 19:24:59 +0000

    end of day

--- todo/test_views.py
+++ todo/test_views.py
@@ -7,11 +7,4 @@ class TestDjango(TestCase):
     def test_this_thing_works(self):
         self.assertEqual(1,1)
 
-    def test_this_thing_works_2(self):
-        self.assertEqual(1,1)
-
-    def test_this_thing_works_3(self):
-        self.assertEqual(3,3)
-
-    def test_this_thing_works_4(self):
-        self.assertEqual(1,1)
+    
