72ddc218a536f41ae61ef99a878cae5028cc2036
joffy69 <jcwgibbs@gmail.com>
2020-12-20 17:54:22 +0000

    tests set up added

---
+++ todo/test_views.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class TestDjango(TestCase):
+
+    def test_this_thing_works(self):
+        self.assertEqual(1,1)
+
+    def test_this_thing_works_2(self):
+        self.assertEqual(1,1)
+
+    def test_this_thing_works_3(self):
+        self.assertEqual(3,3)
+
+    def test_this_thing_works_4(self):
+        self.assertEqual(1,1)
